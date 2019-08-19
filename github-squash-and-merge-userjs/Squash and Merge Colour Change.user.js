72b10c73ee91af4b020a8135606db34cd32c27c6
Robert Hawdon <git@robertianhawdon.me.uk>
2019-08-19 23:06:14 +0100

    Updated jquery version and updated script for new GitHub code

--- github-squash-and-merge-userjs/Squash and Merge Colour Change.user.js
+++ github-squash-and-merge-userjs/Squash and Merge Colour Change.user.js
@@ -1,20 +1,21 @@
 // ==UserScript==
 // @name         Squash and Merge Colour Change
 // @namespace    https://robertianhawdon.me.uk/
-// @version      0.5
+// @version      0.6
 // @description  Makes it more obvious you're doing a squash... Стоян!
 // @author       Robert Ian Hawdon
 // @match        https://github.com/*
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        GM_addStyle
 // ==/UserScript==
 
-waitForKeyElements (".btn-group-squash > button.btn", actionFunction);
+waitForKeyElements (".btn-group-squash, .BtnGroup-item > button.btn", actionFunction);
+waitForKeyElements (".btn-group-squash > button.btn, .js-merge-commit.button", actionFunction);
 
 
 function actionFunction (jNode) {
 
-    jNode.removeClass ("btn-primary").addClass ("btn-blue");
+    jNode.removeClass("btn-primary").addClass("btn-blue");
 
 }
